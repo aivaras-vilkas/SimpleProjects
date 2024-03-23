58335089ad7689aa3d1ff085605434f7b1c6ac3d
Vilks <vilkas.aivaras@gmail.com>
2024-03-23 18:03:06 +0200

    Added Simple Calculator code

---
+++ ConsolePrograms/SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿while (true)
+{
+Console.WriteLine("********Welcome To Simple Calculator********");
+Console.WriteLine("Enter First number");
+double firstNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter Second number");
+double secondNumber = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Chosen an operator: (+, -, *, /)");
+string chosenOperator = Console.ReadLine();
+string wantToRestart;
+
+
+    if (chosenOperator.Length > 1)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Error: INCORRECT OPERATOR !!!");
+        Console.WriteLine("Enter valid operator:");
+        chosenOperator = Console.ReadLine();
+    }
+
+    switch (chosenOperator)
+    {
+        case "+":
+            Console.WriteLine(firstNumber + secondNumber);
+            break;
+        case "-":
+            Console.WriteLine(firstNumber - secondNumber);
+            break;
+        case "*":
+            Console.WriteLine(firstNumber * secondNumber);
+            break;
+        case "/":
+            Console.WriteLine(firstNumber / secondNumber);
+            break;
+    }
+
+    Console.WriteLine("Want to restart the calculator?");
+    wantToRestart = (Console.ReadLine()).ToLower();
+    if (wantToRestart != "taip")
+    {
+        break;
+    }
+}
+
